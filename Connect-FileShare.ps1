4200a293eda9e3652b3f1afb440729fdb74c9bc8
ropy1971 <rodolphe@petrowsky.fr>
2019-03-15 13:42:02 +0100

    Modifications

--- Connect-FileShare.ps1
+++
@@ -1,3 +0,0 @@
-$acctKey = ConvertTo-SecureString -String "WHCoVWnrOFEL2ANllzyGTiZJ2jj8xMIIGkxz07K/aJq8mBdZ3m0dp5zDJo9ln3dfUQk3SyjV/69AWgufl97A/A==" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\frazrfsa", $acctKey
-New-PSDrive -Name S -PSProvider FileSystem -Root "\\frazrfsa.file.core.windows.net\azrfsaftpworkspace" -Credential $credential -Persist
