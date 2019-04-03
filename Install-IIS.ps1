8b0666a1cbc1b2b5722d0d64574f55fed29095ff
ropy1971 <rodolphe@petrowsky.fr>
2019-04-03 14:31:49 +0200

    Modifications.

--- Install-IIS.ps1
+++ Install-IIS.ps1
@@ -1,4 +1,4 @@
-Install-WindowsFeatures -Name Web-Server,Web-WebServer, `
+Install-WindowsFeature -Name Web-Server,Web-WebServer, `
                         Web-Common-Http,Web-Default-Doc,Web-Dir-Browsing,Web-Http-Errors, `
                         Web-Static-Content,Web-Health,Web-Http-Logging,Web-Performance,Web-Stat-Compression, `
                         Web-Security,Web-Filtering,Web-App-Dev,Web-Net-Ext,Web-Net-Ext45,Web-AppInit, `
