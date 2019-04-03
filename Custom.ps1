3d5be157eef757e077cf5c931c37e8b456fbbbd4
ropy1971 <rodolphe@petrowsky.fr>
2019-04-03 15:34:47 +0200

    Modifications.

---
+++ Custom.ps1
@@ -0,0 +1,8 @@
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask 
+
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000
