018486399cdbe30db88955ea0dcc1fa263deb245
ropy1971 <rodolphe@petrowsky.fr>
2019-04-03 14:25:39 +0200

    Initial commit.

---
+++ Install-IIS.ps1
@@ -0,0 +1,6 @@
+Install-WindowsFeatures -Name Web-Server,Web-WebServer, `
+                        Web-Common-Http,Web-Default-Doc,Web-Dir-Browsing,Web-Http-Errors, `
+                        Web-Static-Content,Web-Health,Web-Http-Logging,Web-Performance,Web-Stat-Compression, `
+                        Web-Security,Web-Filtering,Web-App-Dev,Web-Net-Ext,Web-Net-Ext45,Web-AppInit, `
+                        Web-ASP,Web-Asp-Net,Web-Asp-Net45,Web-CGI,Web-ISAPI-Ext,Web-ISAPI-Filter, `
+                        Web-Includes,Web-WebSockets,Web-Mgmt-Tools,Web-Mgmt-Compat,Web-Metabase
