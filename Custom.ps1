b69bdbb0b7c4b079d69287d188e93dafb6e6664e
ropy1971 <rodolphe@petrowsky.fr>
2019-04-10 17:37:14 +0200

    Modifications

--- Custom.ps1
+++ Custom.ps1
@@ -14,6 +14,14 @@ Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Syste
 Set-Service -Name NetTcpPortSharing -StartupType Automatic 
 Start-Service -Name NetTcpPortSharing 
 
+# installation de IIS (web-server) 
+Install-WindowsFeature -Name Web-Server,Web-WebServer, `
+                        Web-Common-Http,Web-Default-Doc,Web-Dir-Browsing,Web-Http-Errors, `
+                        Web-Static-Content,Web-Health,Web-Http-Logging,Web-Performance,Web-Stat-Compression, `
+                        Web-Security,Web-Filtering,Web-App-Dev,Web-Net-Ext,Web-Net-Ext45,Web-AppInit, `
+                        Web-ASP,Web-Asp-Net,Web-Asp-Net45,Web-CGI,Web-ISAPI-Ext,Web-ISAPI-Filter, `
+                        Web-Includes,Web-WebSockets,Web-Mgmt-Tools,Web-Mgmt-Compat,Web-Metabase
+
 # ajout des ports Firewall 
 New-NetFirewallRule -DisplayName "Allow inbound Port 80" -Direction Inbound -LocalPort 80 -Protocol TCP -Action Allow 
 New-NetFirewallRule -DisplayName "Allow inbound Port 443" -Direction Inbound -LocalPort 443 -Protocol TCP -Action Allow 
@@ -22,10 +30,7 @@ New-NetFirewallRule -DisplayName "Allow inbound Port 4515" -Direction Inbound -L
 New-NetFirewallRule -DisplayName "Allow inbound Port 4520" -Direction Inbound -LocalPort 4520 -Protocol TCP -Action Allow
 New-NetFirewallRule -DisplayName "Allow inbound Port 20000" -Direction Inbound -LocalPort 20000 -Protocol TCP -Action Allow 
 
-# suppression de l'expiration du password du compte Admin
-# $Command = "cmd.exe wmic UserAccount where Name='adminroot' set PasswordExpires=False"
-# Invoke-Command -Command:$Command
-
+# Initialisation du disque de données
 $disks = Get-Disk | Where-Object partitionstyle -eq 'raw' | Sort-Object number
 $letters = 70..89 | ForEach-Object { [char]$_ }
 $count = 0
@@ -42,6 +47,8 @@ foreach ($disk in $disks) {
 
 # création du répertoire pour copier les sources en local 
 New-Item -Path "C:\" -Name "Sources" -ItemType "directory"
+New-Item -Path "C:\Sources" -Name "SQLEXPRESS2017" -ItemType "directory"
+New-Item -Path "C:\Sources" -Name "SSSMS" -ItemType "directory"
 
 # montage du Azure File Share
 $acctKey = ConvertTo-SecureString -String "rSIC4O6I+ynbkA8SQfHijBF6SIORKNpNpplzVO3U866/EhpNnpMLu3rZT3M2CZl6KUA68BZq6et+acOI+XkD0g==" -AsPlainText -Force
@@ -49,16 +56,6 @@ $credential = New-Object System.Management.Automation.PSCredential -ArgumentList
 New-PSDrive -Name S -PSProvider FileSystem -Root "\\logepalsa.file.core.windows.net\sources" -Credential $credential -Persist
 
 # copie des sources
-Copy-Item "S:\SQLEXPRESS2017\*.*" -Destination "C:\Sources" -Force
-Copy-Item "S:\SSMS\*.*" -Destination "C:\Sources" -Force
+Copy-Item "S:\SQLEXPRESS2017\*.*" -Destination "C:\Sources\SQLEXPRESS2017" -Force
+Copy-Item "S:\SSMS\*.*" -Destination "C:\Sources\SSMS" -Force
 Copy-Item "S:\SCRIPTS\*.*" -Destination "C:\Sources" -Force
-
-# déconnexion du map S: 
-
-# installation de IIS (web-server) 
-Install-WindowsFeature -Name Web-Server,Web-WebServer, `
-                        Web-Common-Http,Web-Default-Doc,Web-Dir-Browsing,Web-Http-Errors, `
-                        Web-Static-Content,Web-Health,Web-Http-Logging,Web-Performance,Web-Stat-Compression, `
-                        Web-Security,Web-Filtering,Web-App-Dev,Web-Net-Ext,Web-Net-Ext45,Web-AppInit, `
-                        Web-ASP,Web-Asp-Net,Web-Asp-Net45,Web-CGI,Web-ISAPI-Ext,Web-ISAPI-Filter, `
-                        Web-Includes,Web-WebSockets,Web-Mgmt-Tools,Web-Mgmt-Compat,Web-Metabase
